61392507c80398173bb07bc0a5a8551338401407
fraten <59165445+fraten@users.noreply.github.com>
2020-01-18 13:47:33 +0100

    Update image.c

--- src/image.c
+++ src/image.c
@@ -262,8 +262,8 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
                     strcat(labelstr, ", ");
                     strcat(labelstr, names[j]);
                 }
-                printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
-                if (names[j] == 'person') { printf("%d ",1); }
+                //printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
+                //if (names[j] == 'person') { printf("%d ",1); }
             }
         }
         if(class >= 0){
@@ -289,7 +289,7 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
             rgb[1] = green;
             rgb[2] = blue;
             box b = dets[i].bbox;
-           // printf("%.20f %.20f %.20f %.20f\n", b.x, b.y, b.w, b.h);
+            printf("%1d %.20f %.20f %.20f %.20f\n", 1, b.x, b.y, b.w, b.h);
             
             
             //fprintf(ENTREE, "%.20f %.20f %.20f %.20f\n", b.x, b.y, b.w, b.h);
