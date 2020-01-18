a804e20ca70b20830561e108e7ce920e2fd8d0c2
fraten <59165445+fraten@users.noreply.github.com>
2020-01-18 11:56:29 +0100

    Update image.c

--- src/image.c
+++ src/image.c
@@ -262,8 +262,8 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
                     strcat(labelstr, ", ");
                     strcat(labelstr, names[j]);
                 }
-                //printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
-             if (names[j]== "person") {  printf ("%d ",1); }
+                printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
+                if (names[j] == "person") { printf("%d ",1); }
             }
         }
         if(class >= 0){
